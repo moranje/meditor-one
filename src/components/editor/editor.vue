827cc63dff84ac0e3a5271bf39d388854476b708
Martien Oranje <martieno@gmail.com>
2018-03-25 17:27:15 +0200

    Update to Monaco 0.11.1 in a hacky way

--- src/components/editor/editor.vue
+++ src/components/editor/editor.vue
@@ -6,27 +6,8 @@
 </template>
 
 <script>
-import * as monaco from '@timkendrick/monaco-editor';
+import * as monaco from './monaco';
 import editorSettings from './editor-settings';
-
-// window.MonacoEnvironment = {
-//   getWorkerUrl: function(moduleId, label) {
-//     console.log(arguments);
-//     if (label === 'json') {
-//       return './json.worker.js';
-//     }
-//     if (label === 'css') {
-//       return './css.worker.js';
-//     }
-//     if (label === 'html') {
-//       return './html.worker.js';
-//     }
-//     if (label === 'typescript' || label === 'javascript') {
-//       return './ts.worker.js';
-//     }
-//     return './editor.worker.js';
-//   }
-// };
 
 export default {
   name: '',
