a5332cdf8052940bf5a571b639a9844a5151b2b3
Martien Oranje <martieno@gmail.com>
2018-04-25 00:34:26 +0200

    Add test for nested snippets

--- tests/unit/snippets/expansion-snippet.spec.ts
+++ tests/unit/snippets/expansion-snippet.spec.ts
@@ -12,7 +12,30 @@ describe('expansion-snippet.ts', () => {
       ])
     );
 
-    // expect(snippet.expand().serialize()).toBe('$1 $2 $3');
     expect(snippet.expand().tabstops.serialize()).toBe('1:$1 2:$2 3:$3');
+  });
+
+  it('should correctly expand nested placeholders', () => {
+    let list = new SnippetList('${!expansion} ${1:first}', [
+      {
+        name: 'expansion',
+        value: '$1 ${2:second ${3:third}}'
+      }
+    ]);
+    let snippet = new Expansion('${!expansion}', list);
+
+    expect(list.serialize()).toBe('$2 ${3:second ${4:third}} ${1:first}');
+  });
+
+  it('should correctly expand nested placeholders', () => {
+    let list = new SnippetList('${!expansion} ${1:first}', [
+      {
+        name: 'expansion',
+        value: '$1 ${2:second $3}'
+      }
+    ]);
+    let snippet = new Expansion('${!expansion}', list);
+
+    expect(list.serialize()).toBe('$2 ${3:second $4} ${1:first}');
   });
 });
