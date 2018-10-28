2e44855e1ade31763331bc312dcc77609b37ae8e
Martien Oranje <martieno@gmail.com>
2018-10-28 22:22:01 +0100

    build(project): switch to cz-customizable adapter

--- .cz-config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  scopes: ['parser', 'ui', 'tests', 'project', 'storage'],
-  allowCustomScopes: false
-};
---
+++ cz-config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  types: [
+    { value: 'feat', name: 'feat:     A new feature' },
+    { value: 'fix', name: 'fix:      A bug fix' },
+    { value: 'docs', name: 'docs:     Documentation only changes' },
+    {
+      value: 'style',
+      name:
+        'style:    Changes that do not affect the meaning of the code\n            (white-space, formatting, missing semi-colons, etc)'
+    },
+    {
+      value: 'refactor',
+      name:
+        'refactor: A code change that neither fixes a bug nor adds a feature'
+    },
+    {
+      value: 'perf',
+      name: 'perf:     A code change that improves performance'
+    },
+    { value: 'test', name: 'test:     Adding missing tests' },
+    {
+      value: 'build',
+      name:
+        'build:    Changes that affect the build system or external dependencies (example scopes: gulp, broccoli, npm)'
+    },
+    {
+      value: 'ci',
+      name:
+        'ci:       Changes to our CI configuration files and scripts (example scopes:Travis, Circle, BrowserStack, SauceLabs)'
+    },
+    {
+      value: 'chore',
+      name:
+        'chore:    Changes to the build process or auxiliary tools\n            and libraries such as documentation generation'
+    },
+    { value: 'revert', name: 'revert:   Revert to a commit' }
+  ],
+  scopes: ['parser', 'ui', 'tests', 'project', 'storage'],
+  allowCustomScopes: false
+};
