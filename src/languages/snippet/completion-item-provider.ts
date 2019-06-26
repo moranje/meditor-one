ea07156b61ed79408a375f482285b38dd780278b
Martien Oranje <martieno@gmail.com>
2019-06-27 00:40:57 +0200

    perf(editor): cache completion items inside snippet editor

--- src/languages/snippet/completion-item-provider.ts
+++ src/languages/snippet/completion-item-provider.ts
@@ -9,6 +9,8 @@ let disposables = {
   init: null,
   changes: null,
 }
+let files = null
+let suggestions = null
 
 export default {
   init(language) {
@@ -29,22 +31,30 @@ export default {
   destroy(editor) {
     if (disposables.init) disposables.init.dispose()
     if (disposables.changes) disposables.changes.dispose()
+    files = null
+    suggestions = null
   },
 } as EditorProvider
 
 function provideCompletionItems() {
-  let files = File.query()
-    .with('parent')
-    .orderBy('name')
-    .all()
-
-  return {
-    suggestions: files.map(file => {
-      return <monaco.languages.CompletionItem>{
-        label: `${slug(file.parent.name)}-${slug(file.name)}`,
-        insertText: `${slug(file.parent.name)}-${slug(file.name)}`,
-        kind: monaco.languages.CompletionItemKind.Snippet,
-      }
-    }),
+  if (!files) {
+    files = File.query()
+      .with('parent')
+      .orderBy('name')
+      .all()
   }
+
+  if (!suggestions) {
+    suggestions = {
+      suggestions: files.map(file => {
+        return <monaco.languages.CompletionItem>{
+          label: `${slug(file.parent.name)}-${slug(file.name)}`,
+          insertText: `${slug(file.parent.name)}-${slug(file.name)}`,
+          kind: monaco.languages.CompletionItemKind.Snippet,
+        }
+      }),
+    }
+  }
+
+  return suggestions
 }
